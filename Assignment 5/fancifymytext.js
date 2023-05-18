5df4dfb0453c2954379972a2544f1ee5a422a111
Josiah <josiahvillarante@gmail.com>
2023-05-18 02:20:02 -0700

    Added functionality to radio buttons. Added moo button to alter text area text.

--- Assignment 5/fancifymytext.js
+++ Assignment 5/fancifymytext.js
@@ -6,3 +6,36 @@ function biggerText() {
     var textArea = document.getElementById("textArea");
     textArea.style.fontSize = "24pt";
 }
+
+function fancyShmancy() {
+    var textArea = document.getElementById("textArea");
+    textArea.style.fontWeight = "bold";
+    if (document.getElementById("fancyRadio").checked) {
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
+    }
+}
+
+function boringBetty() {
+    var textArea = document.getElementById("textArea");
+    if (document.getElementById("boringRadio").checked) {
+        textArea.style.fontWeight = "normal";
+        textArea.style.color = "black";
+        textArea.style.textDecoration = "none";
+    }
+}
+
+function moo() {
+    var textArea = document.getElementById("textArea");
+    var text = textArea.value.toUpperCase();
+    var sentences = text.split(".");
+    var modifiedSentences = sentences.map(function(sentence) {
+        var words = sentence.split(" ");
+        if (words.length > 0) {
+            var lastWord = words[words.length - 1];
+            words[words.length - 1] = lastWord + "-Moo";
+        }
+        return words.join(" ");
+    });
+    textArea.value = modifiedSentences.join(".");
+}
